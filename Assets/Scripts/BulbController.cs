af5cfe4b6c6df459ee0e53c6043a94b220961e7b
Alisa Gavrilova <64360518+GAD-Alisa@users.noreply.github.com>
2021-01-21 10:19:46 +0000

    Unfinished Update1

---
+++ Assets/Scripts/BulbController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulbController : MonoBehaviour
+{
+    public GameObject Bulb;
+    private bool isOn = false;
+    private Animator bulbAnimator;
+    switchOff.GetComponent<ManualSwitchController>();
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SwitchLightOn()
+        SwitchLightOff()
+
+    }
+    
+   
+ 
+    void SwitchLightOn()
+    { 
+       if switchOff == true && isOn == false
+        {
+
+            bulbAnimator.SetBool("IsSwitchOn", true);
+            isOn = true
+        }
+    }
+
+    void SwitchLightOff()
+    { 
+      if switchOff == false && isOn == true
+       {
+           bulbAnimator.SetBool("IsSwitchOn", true);
+           isOn = true
+       }
+    }
+
+
+}
